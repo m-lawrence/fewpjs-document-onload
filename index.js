7e49f1796903395e7c5b0979dd8963912e8230da
Melissa Lawrence <mlawrence537@gmail.com>
2021-04-06 17:56:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(e) {
+    console.log("The DOM has loaded");
+    const changeText = document.querySelector('p#text')
+    changeText.innerHTML = "This is really cool!"
+  });
+  
+  
